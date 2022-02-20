f672a21cfbfe787877b740fc64ae4e014568527c
Rahul Dash <66854826+areyrahul@users.noreply.github.com>
2022-02-20 16:43:54 +0530

    change bg color of page to same color as button

--- user-interface/popup.js
+++ user-interface/popup.js
@@ -1,5 +1,14 @@
-let changeColor = document.getElementById("changeColor");
+changeColor.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-chrome.storage.sync.get("color", ({ color }) => {
-    changeColor.style.backgroundColor = color;
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: setPageBackgroundColor
+    });
 });
+
+function setPageBackgroundColor() {
+    chrome.storage.sync.get("color", ({ color }) => {
+        document.body.style.backgroundColor = color;
+    });
+}
