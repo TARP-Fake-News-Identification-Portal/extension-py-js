3796f95fd5f41dd2f62fd31915a0290f2dcc5770
DrKakku <DrKakku19@gmail.com>
2022-04-27 22:15:40 +0530

    Changed the request file

--- user-interface/popup.js
+++ user-interface/popup.js
@@ -1,13 +1,21 @@
 chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
-  chrome.storage.sync.set({ key1: tab.url });
+    chrome.storage.sync.set({ key1: tab.url });
 });
 
-fetch("http://127.0.0.1:5000/predict", {
-  method: "POST",
-  body: JSON.stringify({
-    data: chrome.storage.sync.get("key1"),
-  }),
-  headers: {
-    "Content-type": "application/json; charset=UTF-8",
-  },
+// fetch("http://127.0.0.1:8080/predict", {
+//     method: "POST",
+//     body: JSON.stringify({
+//         data: chrome.storage.sync.get("key1"),
+//     }),
+//     headers: {
+//         "Content-type": "application/json; charset=UTF-8",
+//     },
+// });
+const axios = require("axios");
+axios({
+    method: 'post',
+    url: 'http://127.0.0.1:8080/predict',
+    data: {
+        data: chrome.storage.sync.get("key1"),
+    }
 });
