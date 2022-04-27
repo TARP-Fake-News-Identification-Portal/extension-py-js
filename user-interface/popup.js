8ceb3e2c4ddc8474eb88fc276170692c4819bbd4
Rahul Dash <66854826+areyrahul@users.noreply.github.com>
2022-04-27 21:43:36 +0530

    commit-2

--- user-interface/popup.js
+++ user-interface/popup.js
@@ -2,4 +2,12 @@ chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
   chrome.storage.sync.set({ key1: tab.url });
 });
 
-chrome.tabs.create("https://www.google.com");
+fetch("http://127.0.0.1:5000/predict", {
+  method: "POST",
+  body: JSON.stringify({
+    data: chrome.storage.sync.get("key1"),
+  }),
+  headers: {
+    "Content-type": "application/json; charset=UTF-8",
+  },
+});
