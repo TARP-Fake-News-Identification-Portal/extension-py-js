b1bd6cb5ed9af4885e18f63609769158fac868d8
Rahul Dash <66854826+areyrahul@users.noreply.github.com>
2022-04-28 02:46:17 +0530

    commit-3

--- user-interface/popup.js
+++ user-interface/popup.js
@@ -1,21 +1,67 @@
-chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
-    chrome.storage.sync.set({ key1: tab.url });
+chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+  username = tabs[0].url.substr(20);
+  //chrome.tabs.create({ url: "https://twitter.com/" + username });
+
+  fetch("http://127.0.0.1:5000/predict", {
+    method: "POST",
+    body: { data: username },
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => {
+      console.log(response.json());
+    })
+    .then((data) => {
+      console.log("Success:", data);
+    })
+    .catch((error) => {
+      console.log("Error:", error);
+    });
 });
 
-// fetch("http://127.0.0.1:8080/predict", {
-//     method: "POST",
-//     body: JSON.stringify({
-//         data: chrome.storage.sync.get("key1"),
-//     }),
-//     headers: {
-//         "Content-type": "application/json; charset=UTF-8",
-//     },
+// const axios = import("axios");
+
+// const predict = (url) => {
+//   axios
+//     .post("http://127.0.0.1:5000/predict", url)
+//     .then((response) => {
+//       const result = response.prediction;
+//       console.log("POST call result", result);
+//     })
+//     .catch((error) => console.error(error));
+// };
+
+// predict({
+//   data: chrome.storage.sync.get("key-1"),
 // });
-const axios = require("axios");
-axios({
-    method: 'post',
-    url: 'http://127.0.0.1:8080/predict',
-    data: {
-        data: chrome.storage.sync.get("key1"),
-    }
-});
+
+// axios({
+//   method: "post",
+//   url: "http://127.0.0.1:5000/predict",
+//   data: {
+//     data: chrome.storage.sync.get("key1"),
+//   },
+// });
+
+// axios
+//   .post("http://127.0.0.1:5000/predict", {
+//     data: chrome.storage.sync.get("key1"),
+//   })
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+// async function makeGetRequest() {
+//   let payload = { data: chrome.storage.sync.get("key1") };
+
+//   let res = await axios.post("http://127.0.0.1:5000/predict", payload);
+
+//   let data = res.data;
+//   console.log(data);
+// }
+
+// makeGetRequest();
