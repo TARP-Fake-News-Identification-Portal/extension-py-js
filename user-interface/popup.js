663ef8ae2ec12385bf1901f0d3645b6cb81fb2cc
Rahul Dash <66854826+areyrahul@users.noreply.github.com>
2022-04-28 19:47:15 +0530

    minor changes

--- user-interface/popup.js
+++ user-interface/popup.js
@@ -13,9 +13,7 @@ function apicall(username) {
   fetch("http://127.0.0.1:5000/predict", {
     method: "POST",
     body: JSON.stringify({
-      title: "foo",
-      body: "bar",
-      userId: 1,
+      title: "username",
       data: username,
     }),
     headers: {
@@ -23,7 +21,7 @@ function apicall(username) {
     },
   })
     .then((response) => {
-      console.log(response);
+      alert(response);
     })
     .then((data) => {
       console.log("Success:", data);
@@ -32,48 +30,3 @@ function apicall(username) {
       console.log("Error:", error);
     });
 }
-// const axios = import("axios");
-
-// const predict = (url) => {
-//   axios
-//     .post("http://127.0.0.1:5000/predict", url)
-//     .then((response) => {
-//       const result = response.prediction;
-//       console.log("POST call result", result);
-//     })
-//     .catch((error) => console.error(error));
-// };
-
-// predict({
-//   data: chrome.storage.sync.get("key-1"),
-// });
-
-// axios({
-//   method: "post",
-//   url: "http://127.0.0.1:5000/predict",
-//   data: {
-//     data: chrome.storage.sync.get("key1"),
-//   },
-// });
-
-// axios
-//   .post("http://127.0.0.1:5000/predict", {
-//     data: chrome.storage.sync.get("key1"),
-//   })
-//   .then(function (response) {
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   });
-
-// async function makeGetRequest() {
-//   let payload = { data: chrome.storage.sync.get("key1") };
-
-//   let res = await axios.post("http://127.0.0.1:5000/predict", payload);
-
-//   let data = res.data;
-//   console.log(data);
-// }
-
-// makeGetRequest();
