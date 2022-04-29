f20d7619d5e9b079087ad22aacce8f7ea73a04d9
Rahul Dash <66854826+areyrahul@users.noreply.github.com>
2022-04-29 11:18:37 +0530

    changes that make sense

--- user-interface/popup.js
+++ user-interface/popup.js
@@ -20,9 +20,26 @@ function apicall(username) {
   })
     .then((response) => response.json())
     .then((data) => {
-      var un = document.getElementById("username");
-      un.appendChild(document.createTextNode(username));
+      // Changing please wait to given user
+      var dispUn = document.getElementById("displayUsername");
+      dispUn.innerHTML =
+        "Given User: <font color='#FF4B51'>" + username + "</font>";
+
+      // fetching table
       var printTable = document.getElementById("printTable");
+
+      // creating table headers
+      var heading = document.createElement("th");
+      var headingText = document.createTextNode("Tweet");
+      heading.appendChild(headingText);
+      printTable.appendChild(heading);
+
+      heading = document.createElement("th");
+      headingText = document.createTextNode("Authenticity");
+      heading.appendChild(headingText);
+      printTable.appendChild(heading);
+
+      // creating rows and columns
       for (var tweet_auth of data.prediction) {
         var row = document.createElement("tr");
         for (var i = 0; i <= 1; i++) {
